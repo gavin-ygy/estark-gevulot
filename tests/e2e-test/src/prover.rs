bf7c56fa87a45c58017ba7fa57848db25bfc3716
[gavin-ygy] <[gavin.ygy@gmail.com]>
2024-06-29 11:30:50 +0800

    update

--- tests/e2e-test/src/prover.rs
+++ tests/e2e-test/src/prover.rs
@@ -370,7 +370,6 @@ fn run_task(task: Task) -> gResult<TaskResult> {
     write!(log_file, "output_path:{}\n",  &args.output_path)?;
 
   
-/*
 
     //generate proof
     let suite_json = fs::read_to_string(args.trace_file).unwrap();
@@ -412,7 +411,7 @@ fn run_task(task: Task) -> gResult<TaskResult> {
         }
         _ => write!(log_file, "The prover executes successfully.\n")?,
     };
-*/
+
     log::info!("The prover executes successfully");
 
     // Write generated proof to a file.
@@ -421,17 +420,15 @@ fn run_task(task: Task) -> gResult<TaskResult> {
 
     let circom_file = format!("{}/{}_chunk_{}.circom",&args.output_path, &args.task_name, &args.chunk_id);
     let proof_file = format!("{}/{}_chunk_{}/{}_proof.bin",&args.output_path, &args.task_name, &args.chunk_id, &args.task_name);
-    //task.result(vec![], vec![String::from(proof_file),String::from(circom_file),String::from("/workspace/test.log")])
-    
-    //#[arg(short, long = "proof_file", default_value = "/workspace/lr_chunk_0/lr_proof.bin")]
-    //#[arg(short, long = "circom_file", default_value = "/workspace/lr_chunk_0.circom")]
+     
+
     //return three files for Verifier
-    let tmp_path = "/workspace/lr_chunk_0";
+   /* let tmp_path = "/workspace/lr_chunk_0";
     fs::create_dir_all(&tmp_path).unwrap();
     std::fs::write("/workspace/lr_chunk_0/lr_proof.bin", b"this is a proof a.");
     std::fs::write("/workspace/lr_chunk_0.circom", b"this is a circom .");
     write!(log_file, "circom out:{}\n",  &circom_file)?;
-    write!(log_file, "proof out:{}\n",  &proof_file)?;
+    write!(log_file, "proof out:{}\n",  &proof_file)?; */
     //task.result(vec![], vec![String::from("/workspace/lr_chunk_0/lr_proof.bin"),String::from("/workspace/lr_chunk_0.circom"),String::from("/workspace/test.log")])
     task.result(vec![], vec![String::from(proof_file),String::from(circom_file),String::from("/workspace/test.log")])
     
